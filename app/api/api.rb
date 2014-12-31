f4edae6cbc718c01e0cf6f64622cc8eadc711463
Aurélien Hervé <mail@aurelien-herve.com>
2014-12-31 15:14:17 +0100

    removed users sign_in

--- app/api/api.rb
+++ app/api/api.rb
@@ -3,13 +3,14 @@ require 'grape-swagger'
 class API < Grape::API
   version :v1, using: :accept_version_header, format: :json, default_format: :json do 
 
-    helpers Devise::Controllers::SignInOut
-    helpers Devise::Controllers::StoreLocation
-    helpers MyApi::V1::Helpers::Session
+    ## Users management
+    #helpers Devise::Controllers::SignInOut
+    #helpers Devise::Controllers::StoreLocation
+    #helpers MyApi::V1::Helpers::Session
+    #mount MyApi::V1::Users
 
     # Mount some api methods
     mount MyApi::V1::Ping
-    mount MyApi::V1::Users
 
     # Generate an api documentation
     add_swagger_documentation(mount_path: '/doc/swagger_doc', base_path: '/api') 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
   mount API => '/api'
   mount GrapeSwaggerRails::Engine => '/api/doc'
 end
