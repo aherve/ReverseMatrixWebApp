62bf50790c34d08e4a6a7a4334ff74ffcf1069fb
Aurélien Hervé <mail@aurelien-herve.com>
2015-01-15 15:35:17 +0100

    land mailer

--- app/mailers/land_mailer.rb
+++ app/mailers/land_mailer.rb
@@ -2,15 +2,21 @@ class LandMailer < ActionMailer::Base
   default from: "aurelien@woodstack.io"
 
   def new_interesting_land(land)
-    @url = land.url
-    @price = land.price_in_euro
-    @surface = land.surface_in_squared_meters
+    @url       = land.url
+    @price     = land.price_in_euro
+    @surface   = land.surface_in_squared_meters
     @town_name = land.town_readable_name
+    @traject   = land.town.car_travel_time_text
 
-    mail(to: "woodstack@woodstack.io", subject: '[recherche terrain] nouvelle annonce intéressante')
+    mail(to: "woodstack@woodstack.io", subject: '[recherche terrain] annonce intéressante')
   end
 
   def new_land(land)
+    @url       = land.url
+    @price     = land.price_in_euro
+    @surface   = land.surface_in_squared_meters
+    @town_name = land.town_readable_name
+    @traject   = land.town.car_travel_time_text
     mail(to: "woodstack@woodstack.io", subject: '[recherche terrain] nouvelle annonce de terrain disponible!')
   end
 
