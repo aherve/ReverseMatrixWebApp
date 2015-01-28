ac932d6f5c43d5ee478ce0c099f9a7a9c2a5c232
Aurélien Hervé <mail@aurelien-herve.com>
2015-01-28 11:47:51 +0100

    added town to project entity

--- app/api/my_api/entities/project.rb
+++ app/api/my_api/entities/project.rb
@@ -6,6 +6,8 @@ module MyApi
       expose :new_lands_count
       expose :archived_lands_count
       expose :favorite_lands_count
+
+      expose :town, using: MyApi::Entities::Town
     end
   end
 end
