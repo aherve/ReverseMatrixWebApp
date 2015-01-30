4e599862951344578d7e6b436b3c49bbe0f5d51d
Aurélien Hervé <mail@aurelien-herve.com>
2015-01-30 19:32:27 +0100

    this should be tested

--- lib/lands_scrapper/se_loger/scrapper.rb
+++ lib/lands_scrapper/se_loger/scrapper.rb
@@ -17,25 +17,28 @@ module LandsScrapper
       end
 
       def get_page(dep,i)
-        Nokogiri::HTML(
-          open(
-            "http://www.seloger.com/list.htm?cp=#{dep}&idtt=2&idtypebien=4.json&LISTING-LISTpg=#{i}&surfacemin=#{@min_surface}"
-          )
+        LandsScrapper::SeLoger::Page.new(
+          "http://www.seloger.com/list.htm?cp=#{dep}&idtt=2&idtypebien=4.json&LISTING-LISTpg=#{i}&surfacemin=#{@min_surface}"
         )
       end
 
       def number_of_pages(dep)
-        text = @pages[dep][1].xpath(".//p[contains(@class,'pagination_result_number')]").text
-        nb_annonces = text.split(" ").first.to_i
-        nb = begin
-               i,j = nb_annonces_per_page = text.split(" ").last.scan(/([0-9]+)/).map(&:first).map(&:to_i)
-               nb_annonces_per_page = j - i + 1
-               (nb_annonces / nb_annonces_per_page.to_f).ceil
-             rescue
-               0
-             end
-        puts "[seloger.com]: found #{nb} pages of results in department #{dep}" 
-        nb
+        @nb_of_pages ||= Hash.new do |h,dep|
+          h[dep] = (
+            text = @pages[dep][1].content.xpath(".//p[contains(@class,'pagination_result_number')]").text
+            nb_annonces = text.split(" ").first.to_i
+            nb = begin
+                   i,j = nb_annonces_per_page = text.split(" ").last.scan(/([0-9]+)/).map(&:first).map(&:to_i)
+                   nb_annonces_per_page = j - i + 1
+                   (nb_annonces / nb_annonces_per_page.to_f).ceil
+                 rescue
+                   0
+                 end
+            puts "[seloger.com]: found #{nb} pages of results in department #{dep}" 
+            nb
+          )
+        end
+        @nb_of_pages[dep]
       end
 
       def all_pages
@@ -44,71 +47,6 @@ module LandsScrapper
             pages[dep][i]
           end
         end
-      end
-
-      # extract lands from page
-      def to_lands
-        @lands ||= lambda { |nokogiri_page|
-          nokogiri_page.xpath(".//article[contains(@class,'listing')]").map do |p|
-            h = {
-              provider: :seloger,
-              price_in_euro: (p.xpath(".//a[contains(@class,'amount')]").first.text.gsub(/\D/,'').to_i rescue nil),
-              locality: (p.xpath(".//h2//span").text rescue nil),
-              surface_in_squared_meters: (to_squared_meters(p.xpath(".//ul[contains(@class,'property_list')]//li").map(&:text).select{|s| s.match(/ha\z/) or URI.encode(s).match(/[0-9]+m/)}.first ) rescue nil),
-              description: (p.xpath(".//p[contains(@class,'description')]").first.text.chomp.strip rescue nil),
-              url: (p.xpath(".//h2//a").first.attributes["href"].value rescue nil),
-              img: (p.xpath(".//div[contains(@class,'listing_photo_container')]//img").first.attributes["src"].value rescue nil),
-            } 
-          end
-        }
-      end
-
-      def to_squared_meters(text)
-        return nil if text.nil?
-        if text.match(/ha\z/) #result in hectares
-          (text.gsub(',','.').split(" ").first.to_f * 10_000).to_i
-        else
-          text.to_i
-        end
-      end
-
-      def with_town
-        @with_town ||= lambda {|h|
-          h.merge({
-            town_id: (Town.find_by_approximate_name(h[:locality]).id rescue nil)
-          })
-        }
-      end
-
-      def to_land
-        @to_land ||= lambda {|h|
-          Land.new(
-            h.slice(
-              :provider,
-              :price_in_euro,
-              :surface_in_squared_meters,
-              :description,
-              :url,
-              :town_id,
-              :img,
-            )
-          )
-        }
-      end
-
-      def lands
-        all_pages
-        .flat_map(&to_lands)
-        .map(&with_town)
-        .map(&to_land)
-      end
-
-      def new_lands
-        all_pages
-        .flat_map(&to_lands)
-        .reject{|h| Land.where(url: h[:url]).exists?}
-        .map(&with_town)
-        .map(&to_land)
       end
 
     end
--- lib/tasks/lands.rake
+++ lib/tasks/lands.rake
@@ -6,8 +6,8 @@ namespace :lands do
     puts "min_surf=#{@min_surf}"
     #terrain-construction
     puts "fetching lands from terrain-construction.com"
-    nl = LandsScrapper::TerrainConstruction::Scrapper.new(min_surface: @min_surf, max_dist_from_paris: 900).new_lands
-    nl.each{|l| puts "new land in #{l.town.readable_name}: #{l.surface_in_squared_meters}m² for #{l.price_in_euro} euros" if l.save and l.town_id.present?}
+    tc = LandsScrapper::TerrainConstruction::Scrapper.new(min_surface: @min_surf, max_dist_from_paris: 900)
+    tc.all_pages.each{|p| p.delay.save_new_lands!}
 
   end
 
@@ -16,8 +16,8 @@ namespace :lands do
     #se loger.com
     puts "min_surf=#{@min_surf}"
     puts "fetching lands from seloger.com"
-    nl = LandsScrapper::SeLoger::Scrapper.new(min_surface: @min_surf).new_lands
-    nl.each{|l| puts "new land in #{l.town.readable_name}: #{l.surface_in_squared_meters}m² for #{l.price_in_euro} euros" if l.save and l.town_id.present?}
+    sl = LandsScrapper::SeLoger::Scrapper.new(min_surface: @min_surf)
+    sl.all_pages.each{|p| p.delay.save_new_lands!}
   end
 
   desc "scrap the ouèbe to find new lands"
